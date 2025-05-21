e89c1a0c36bc935d74cca395f69230dfb503380f
wshamroukh <wshamroukh@gmail.com>
2025-05-21 19:27:49 +0400

    update urls

--- etc/checkBaseUrls.sh
+++ etc/checkBaseUrls.sh
@@ -11,10 +11,10 @@ echo "Base template URL: $BASE_TEMPLATE_URL"
 SCRIPT_LOCATION=https://raw.githubusercontent.com/Azure/Moodle/$CURRENT_BRANCH/scripts/
 echo "Script location: $SCRIPT_LOCATION"
 
-DEPLOY_TO_AZURE_URL=https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2FMoodle%2F$CURRENT_BRANCH%2Fazuredeploy.json
+DEPLOY_TO_AZURE_URL=https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fwshamroukh%2FMoodle%2F$CURRENT_BRANCH%2Fazuredeploy.json
 echo "Deploy to Azure URL: $DEPLOY_TO_AZURE_URL"
 
-VISUALIZE_URL=http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2FAzure%2FMoodle%2F$CURRENT_BRANCH%2Fazuredeploy.json
+VISUALIZE_URL=http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Fwshamroukh%2FMoodle%2F$CURRENT_BRANCH%2Fazuredeploy.json
 echo "Visualize template URL: $VISUALIZE_URL"
 
 # Check values in README.md
